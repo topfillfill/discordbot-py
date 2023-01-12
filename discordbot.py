92ce32a92379464b712ffc66fa5d94abfe497167
topfillfill <122503437+topfillfill@users.noreply.github.com>
2023-01-12 16:10:57 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,29 +1,33 @@
+from cmath import log
+from distutils.sysconfig import PREFIX
 import discord
-from discord.ext import commands
-bot = commands.Bot(command_prefix='',intents=discord.Intents.all())
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
-@bot.event
-async def on_message():
-    await bot.change_presence(status=discord.status.online, activity=discord.Game('강의'))
-    print('디스코드 봇이 열렸다!')
+PREFIX = os.environ['PREFIX']
+TOKEN = os.environ['TOKEN']
 
-@bot.event
-async def on_message(msg):
-    if msg.author.bot: return None
-    await bot.process_commands(msg)
+client = discord.Client()
 
-@bot.command()
-async def 야(ctx):
-    await ctx.channel.send('호!')
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user}.')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content == f'{PREFIX}호!':
+        await message.channel.send("야")
+
+    if message.content.startswith(f'{PREFIX}야'):
+        await message.channel.send('호!')
+        
 
 
-@bot.event
-async def on_message(msg):
-    if msg.author.bot: return None
-    await bot.process_commands(msg)
-
-@bot.command()
-async def 사랑해(ctx):
-    await ctx.channel.send('나도')
-
-bot.run("MTA2MjkyOTcyODY4NTc1MjM1MA.GJxq7b.7e2l2Pw0hUBaHa5L3aZ6IG9BNGMAFJI0uUa4uk")
+try:
+    client.run(TOKEN)
+except discord.errors.LoginFailure as e:
+    print("Improper token has been passed.")
